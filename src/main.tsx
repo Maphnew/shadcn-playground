54344e27f087c132217d6aefac7a2da700bb6d76
sungchan.kim <sungchan.kim@datastreams.co.kr>
2024-11-07 12:18:07 +0900

    react-router-dom 설치 및 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+], { 
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionStatusRevalidation: true,
+      v7_skipActionErrorRevalidation: true,
+    }
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} future={{ v7_startTransition: true }}/>
   </StrictMode>,
 )
